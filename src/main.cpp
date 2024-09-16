dcc916af6078352927f661ccf62c04584d6b2773
赵义森 <solongnight@outlook.com>
2024-09-16 20:40:44 +0800

    可以选择是否使用自定义窗口

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,20 @@
 ﻿#include <QApplication>
 #include "MainWindow.h"
-
-#include <qDebug>
-#include <QTextStream>
 #include "CustomWindow.h"
+
+// #define CUSTOM_WINDOW
+
 int main(int argc, char* argv[])
 {
-
-#if 1
     QApplication a(argc, argv);
-
+#ifdef CUSTOM_WINDOW
     CustomWindow w;
-
-    MainWindow mainWindow(&w);
+    MainWindow   mainWindow(&w);
     w.setCentent(&mainWindow);
     w.show();
+#else
+    MainWindow w;
+    w.show();
+#endif
     return a.exec();
-#endif
-
-#if 0
-    std::cout << "DS";
-    ExpressParser parser;
-    parser.parse("resources/GFC3X1.exp");  // 请替换为你的EXPRESS文件路径
-    parser.printEntities();
-#endif
-    return 0;
 }
