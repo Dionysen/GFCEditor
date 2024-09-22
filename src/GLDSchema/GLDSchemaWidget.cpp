f78b460e60b027ef99cedd98bd1325d3d0c72585
赵义森 <solongnight@outlook.com>
2024-09-22 21:47:56 +0800

    Update GLDSchemaWidget.cpp

--- src/GLDSchema/GLDSchemaWidget.cpp
+++ src/GLDSchema/GLDSchemaWidget.cpp
@@ -14,16 +14,18 @@ GLDSchemaWidget::GLDSchemaWidget(const QString& title, QWidget* parent)
 
     p_schemaTreeView->setHeaderHidden(true);
     p_schemaTreeView->setFocusPolicy(Qt::NoFocus);
-    // 创建根节点
-    p_schemaModel = new GLDTreeModel(this);
-    // 设置子节点缩进距离
     p_schemaTreeView->setIndentation(15);
 
+
+    p_schemaModel = new GLDTreeModel(this);
+    // 设置子节点缩进距离
     createSchemaModel();
 
     p_schemaTreeView->setModel(p_schemaModel);
     p_schemaTreeView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    p_schemaTreeView->collapseAll();
+    p_schemaTreeView->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+    p_schemaTreeView->setHorizontalScrollMode(QAbstractItemView::ScrollPerPixel);
+    p_schemaTreeView->expandToDepth(0);
     p_schemaTreeView->setUniformRowHeights(true);  // 优化性能
 
     p_projectModel = new QStandardItemModel(this);
@@ -50,6 +52,10 @@ void GLDSchemaWidget::createSchemaModel()
 
     GLDTreeNode* root = new GLDTreeNode("Root", nullptr);
 
+    // 预先加载图标
+    QIcon attributeIcon(":/icon/dark/attribute.svg");
+    QIcon entityIcon(":/icon/dark/schema.svg");
+
     for (auto it = data.begin(); it != data.end(); ++it)
     {
         if (it.value()->GetTypeName() != "ENTITY")
@@ -58,16 +64,15 @@ void GLDSchemaWidget::createSchemaModel()
         }
         // 添加父类
         GLDTreeNode* parentItem = new GLDTreeNode(it.value()->GetName(), root);
-        parentItem->setIcon(QIcon(":/icon/dark/schema.svg"));
+        parentItem->setIcon(entityIcon);
         root->addNode(parentItem);
-
 
         // 添加子类
         QStringList subTypes = it.value().dynamicCast<EntityData>()->GetSubTypes();
         for (auto subType : subTypes)
         {
             GLDTreeNode* childItem = new GLDTreeNode(subType, parentItem);
-            childItem->setIcon(QIcon(":/icon/dark/attribute.svg"));
+            childItem->setIcon(attributeIcon);
             parentItem->addNode(childItem);
         }
     }
